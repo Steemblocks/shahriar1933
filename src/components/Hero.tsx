6cceee7456e3f8448f4a4fc58658592e170e61e0
Dhaka Witness <steemblocks@gmail.com>
2025-12-16 14:57:20 +0600

    Image Update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -92,7 +92,7 @@ export default function Hero() {
                 >
                     <div className="relative w-56 h-56 sm:w-64 sm:h-64 md:w-72 md:h-72 lg:w-[340px] lg:h-[340px] rounded-full overflow-hidden border-4 border-white/20 shadow-2xl group flex items-center justify-center bg-[#d13c36]">
                         <Image
-                            src="/profile.jpeg"
+                            src="/profile.png"
                             alt="Shahriar"
                             fill
                             className="object-contain rounded-full will-change-transform relative z-10"
